5058479fd46d61c7f1852218c9d2980b405c3afe
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-05-22 06:47:56 +0600

    Condition.

--- condition.php
+++ condition.php
@@ -3,9 +3,11 @@
 echo '<h2>Conditions and Booleans</h2>';
 $book = 'Quran';
 $arabic = true;
-if($arabic == true) {
-    echo '<h4>You can read the ' . $book;
+if($arabic) {
+    $msg = 'You can read the ' . $book;
 } else {
-    echo '<h4>You cannot read.</h4>';
+    $msg = 'You cannot read' . $book;
 }
+
+echo $msg;
 ?>
