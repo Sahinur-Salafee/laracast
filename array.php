07d58d167535f2e666e428cd64571cfbb37d7123
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-05-23 06:54:40 +0600

    Associative Array in PHP.

--- array.php
+++ array.php
@@ -15,3 +15,32 @@ $cars = ['Tesla', 'Mercedes', 'Audi', 'Ferrari'];
             <li><?php echo $car; ?></li>
         <?php endforeach; ?>
 </ul>
+
+<h2>Associative Array</h2>
+
+<?php
+    $cars = [
+        [
+            "name" => "Tesla",
+            "model" => "Roadster",
+            "website" => 'https://www.tesla.com/roadster'
+        ],
+        [
+            "name" => "Audi",
+            "model" => "Audi A7",
+            "website" => 'https://www.audi-mediacenter.com/en/audi-a7-sportback-12055'
+        ]
+    ]
+?>
+
+<ul>
+    <?php foreach($cars as $car): ?>
+        
+            <li>
+                <a href="<?php echo $car['website'] ?>" target="_blank">
+                    <?php echo $car['name']; ?>
+                </a>
+            </li>
+        
+    <?php endforeach; ?>
+</ul>
