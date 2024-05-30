7b68d1ec97990c34fd5c6fa4e08502fa566bc71a
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-05-30 07:11:28 +0600

    Add markups.

--- session2/view.about.php
+++ session2/view.about.php
@@ -113,7 +113,7 @@
     </header>
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>This is Homepage.</p>
+            <p>This is About Page.</p>
         </div>
     </main>
     </div>
