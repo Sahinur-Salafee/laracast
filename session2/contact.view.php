29a0074dce5e6736055b658846b8d0cdb9b3548c
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-05-30 20:22:17 +0600

    Add new page and page links.

--- session2/contact.view.php
+++ session2/contact.view.php
@@ -113,7 +113,7 @@
     </header>
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>This is About Page.</p>
+            <p>This is Contact Page.</p>
         </div>
     </main>
     </div>
