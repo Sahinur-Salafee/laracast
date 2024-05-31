4764638b36c754233c1497172f35759f01a98520
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-05-31 08:05:55 +0600

    Dynamic the title as page wise.

--- session2/views/contact.view.php
+++ session2/views/contact.view.php
@@ -1,10 +1,7 @@
     <?php require('views/partials/header.php'); ?>
     <?php require('views/partials/nav.php'); ?>
-    <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
-        </div>
-    </header>
+    <?php require('views/partials/banner.php'); ?>
+    
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <p>This is Contact Page.</p>
--- session2/views/index.view.php
+++ session2/views/index.view.php
@@ -2,6 +2,7 @@
     <?php require('views/partials/header.php'); ?>
     <?php require('views/partials/nav.php'); ?>
 
+    <?php require('views/partials/banner.php'); ?>
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <p>This is Homepage.</p>
