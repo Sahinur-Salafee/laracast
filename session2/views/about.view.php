24397918ae6a604d30062fa35048459ad6f5c198
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-05-31 07:52:49 +0600

    Rearranged files.

---
+++ session2/views/about.view.php
@@ -0,0 +1,15 @@
+    <?php require('views/partials/header.php'); ?>
+    <?php require('views/partials/nav.php'); ?>
+
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <p>This is About Page.</p>
+        </div>
+    </main>
+
+    <?php require('views/partials/footer.php'); ?>
---
+++ session2/views/index.view.php
@@ -0,0 +1,11 @@
+
+    <?php require('views/partials/header.php'); ?>
+    <?php require('views/partials/nav.php'); ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <p>This is Homepage.</p>
+        </div>
+    </main>
+    
+    <?php require('views/partials/footer.php'); ?>
