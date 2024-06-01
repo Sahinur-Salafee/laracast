8f2f672ca6a1eaae28425056d2e72c874f9bcef1
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-06-01 20:24:39 +0600

    Remove code.

--- session2/index.php
+++ session2/index.php
@@ -5,15 +5,9 @@ require 'functions.php';
 $uri = $_SERVER['REQUEST_URI'];
 echo $uri;
 
-var_dump($uri);
 
-echo '<pre>';
-print_r($_SERVER);
-echo '</pre>';
-
-
-if($uri === '/laracast/session2/index.php') {
-    require '/laracast/session2/controllers/index.php';
+if($uri === 'http://localhost/laracast/session2/index.php') {
+    require 'http://localhost/laracast/session2/controllers/index.php';
 } elseif($uri === '/laracast/session2/about.php') {
     require '/laracast/session2/controllers/about.php';
 } elseif($uri === '/laracast/session2/contact.php') {
