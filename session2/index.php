d5ead305cd9e4901b1c92aa8e12aaf5b14a94df1
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-06-03 06:27:42 +0600

    Remove Errors

--- session2/index.php
+++ session2/index.php
@@ -4,13 +4,3 @@ require 'functions.php';
 
 $url = $_SERVER['REQUEST_URI'];
 echo $url;
-
-if($url === '/session2/') {
-    require 'controllers/index.php';
-} elseif($url === '/session2/about') {
-    require 'controllers/about.php';
-} elseif($url === '/session2/contact') {
-    require 'controllers/about.php';
-} else {
-    require 'controllers/404.php';
-}
--- session2/views/index.view.php
+++ session2/views/index.view.php
@@ -1,12 +1,12 @@
+<?php require 'partials/header.php'; ?>
+<?php require 'partials/nav.php'; ?>
+<?php require 'partials/banner.php'; ?>
 
-    <?php require('./../views/partials/header.php'); ?>
-    <?php require('./../views/partials/nav.php'); ?>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <p>This is Home Page.</p>
+    </div>
+</main>
 
-    <?php require('./../views/partials/banner.php'); ?>
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p>This is Homepage.</p>
-        </div>
-    </main>
-    
-    <?php require('./../views/partials/footer.php'); ?>
+<?php require 'partials/footer.php'; ?>
+
