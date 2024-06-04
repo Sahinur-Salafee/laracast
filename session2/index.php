16ea9cce0eac85727a9763e6c411102903ed9b1d
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-06-04 06:50:02 +0600

    Try to connect server.

--- session2/index.php
+++ session2/index.php
@@ -6,8 +6,16 @@ require 'functions.php';
 
 // connect to database
 
-$dsn = "mysql:host=localhost;port=3307;dbname=laracast;charset=utf8m";
+$dsn = "mysql:host=localhost;port=3307;dbname=laracast;user=root;charset=utf8m";
 
 $pdo = new PDO($dsn);
 
+// Prepare statements.
 $statements = $pdo->prepare("SELECT * FROM `posts`");
+
+// Execute
+$statements-> execute();
+
+$posts = $statements-> fetchAll();
+
+dd($posts);
