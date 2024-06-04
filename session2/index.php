2d4c082da763f9830b6827775a4e8b1cbd1fe2f1
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-06-04 06:41:50 +0600

    Connect database.

--- session2/index.php
+++ session2/index.php
@@ -2,5 +2,12 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
 
+// connect to database
+
+$dsn = "mysql:host=localhost;port=3307;dbname=laracast;charset=utf8m";
+
+$pdo = new PDO($dsn);
+
+$statements = $pdo->prepare("SELECT * FROM `posts`");
