b8dae30d9ff2b8fee7a78bf74df8a444c25a2999
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-06-06 06:27:03 +0600

    Refector code.

--- session2/index.php
+++ session2/index.php
@@ -7,39 +7,46 @@ require 'functions.php';
 // connect to database
 
 // Database configuration
-$host = 'localhost';  // Database host
-$port = '3306';       // Database port
-$db = 'laracast';     // Database name
-$user = 'root';       // Database username
-$pass = '';           // Database password
-$charset = 'utf8mb4'; // Character set
+class Database {
 
-// Data Source Name (DSN)
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+    public $connection;
+    public function __construct()
+    {
+        $host = 'localhost';  // Database host
+        $port = '3306';       // Database port
+        $db = 'laracast';     // Database name
+        $user = 'root';       // Database username
+        $pass = '';           // Database password
+        $charset = 'utf8mb4'; // Character set
 
-try {
-    // Create a PDO instance (connect to the database)
-    $pdo = new PDO($dsn, $user, $pass);
-    echo "Database connection successful!<br>";
+        // Data Source Name (DSN)
+        $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
-    // Prepare statements
-    $statements = $pdo->prepare("SELECT * FROM posts");
-
-    // Execute
-    $statements->execute();
-
-    // Fetch all posts
-    $posts = $statements->fetchAll();
-
-    // Output the fetched posts
-    foreach ($posts as $post) {
-        echo $post['title'] . "<br>";
+        // Create a PDO instance (connect to the database)
+        $this-> connection = new PDO($dsn, $user, $pass);
     }
-} catch (PDOException $e) {
-    // Handle connection errors
-    echo "Connection failed: " . $e->getMessage() . "<br>";
-    echo "Error code: " . (int)$e->getCode();
-    exit();
+    
+    public function query($query) {
+        echo "Database connection successful!<br>";
+
+        // Prepare statements
+        $statements = $this->connection->prepare($query);
+
+        // Execute
+        $statements->execute();
+
+        // Fetch all posts
+       return $statements->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
+
+$database = new Database();
+$posts = $database->query("SELECT * FROM posts");
+
+// Output the fetched posts
+foreach ($posts as $post) {
+    echo '<li>' . $post['title'] . "</li>";
+}
+
 
 dd($posts);
