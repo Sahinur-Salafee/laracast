c76d25f4fdf42da73c55fd55f00782a824748f40
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-06-30 20:40:28 +0600

    Add style for forms.

--- session2/create.php
+++ session2/create.php
@@ -1,15 +1,35 @@
 <?php 
 
 require './../session2/views/partials/header.php';
+require 'functions.php';
 
 ?>
+<div class="mx-auto max-w-7xl py-6">
+    <form>
+        <div class="space-y-12">
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div class="sm:col-span-4">
+                    <div class="col-span-full">
+                        <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
+                        <div class="mt-2">
+                            <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                        </div>
+                    </div>
+            </div>
+            </div>
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        </div>
+    </form>
+</div>
 
-<form class="m-10 max-w-9xl w-300">
-    <p>
-        <label for="note">Description</label>
-    </p>
-    <textarea id="note" name="body" class="w-400"></textarea>
-    <p>
-        <button class="my-5 py-2 px-3 bg-blue-900 text-white rounded">Create</button>
-    </p>
-</form>
+
+<?php
+
+dd($_POST);
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+   echo "Form submitted";
+}
+
+
+require './../session2/views/partials/footer.php';
