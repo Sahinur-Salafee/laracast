fc56051278f64d4ef23d99e72da5c271aa62c3b2
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-07-02 20:22:14 +0600

    add button style.

--- session2/create.php
+++ session2/create.php
@@ -49,6 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
         </div>
     </form>
+    <a href="index.php" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">back</a>
 </div>
 
 
--- session2/index.php
+++ session2/index.php
@@ -30,4 +30,4 @@ foreach ($notes as $note): ?>
     <?php endforeach ?>
 </ul>
 
-<a href="create.php">Create</a>
+<a href="create.php" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</a>
